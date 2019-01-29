49ab90be44d594ddebe36dfa234ff676dc64a1a0
huangbin12138 <834601023@qq.com>
2019-01-29 18:03:44 +0800

    .

--- js/tools.js
+++ js/tools.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class T {
+  /**
+   * @func
+   * @desc 判断一个对象是否为DOM对象
+   * @params {object} element - 要判断的对象或变量
+   * @returns {boolean}
+   * */
   isDom(element) {
     return element && typeof element === 'object' && element.nodeType === 1 && typeof element.nodeName === 'string';
   }
 
+  /**
+   * @func
+   * @desc 生成随机数字
+   * @params {number} max=1 - 最大值，默认值为1
+   * @params {number} min=0 - 最小值，默认值为0
+   * @params {number} fixed=0 - 小数点位数，默认值为0
+   * @returns {string}
+   * */
   rNumber(max = 1, min = 0, fixed = 0) {
     max < min && (max = [min, min = max][0]);
     return (Math.random() * (max - min) + min).toFixed(fixed);
   }
 
+  /**
+   * @func
+   * @desc 生成随机字符串
+   * @params {number} len=1 - 字符串长度，默认值为1
+   * @params {string} type='a' - 生成字符串包含字符类型（数字，字母（区分大小写），汉字），默认值为a
+   * @params {string} otherStr='' - 除上术类型中字符类型外包含的字符
+   * @params {boolean} canRepeat=true - 字符是否可以重复， 默认可以重复
+   * @returns {string}
+   * @example
+   * rString(3, '0a', '*') //生成的字符串长度为3，可能包含数字（type中有数字），小写字母（type中有小写字母）和‘*’号
+   * */
   rString(len, type, otherStr, canRepeat = true) {
     typeof type !== 'string' && (type = 'a');
     if (typeof otherStr === 'string') {
@@ -48,6 +73,16 @@ class T {
     return str.filter(e => e).join('');
   }
 
+  /**
+   * @function
+   * @desc 生成随机颜色
+   * @params {number} min=0 - r,g,b值的最小值，默认为0
+   * @params {number} max=255 - r,g,b值的最大值，默认为255
+   * @params {string} type - 返回颜色值类型，可能值rgb | hex，其它匀返回[r, g, b]
+   * @params {string/array}
+   * @example
+   * rColor() // [32, 23, 200]
+   * */
   rColor(min = 0, max = 255, type = '') {
     min > max && (min = [max, max = min][0]);
     min = min < 0 ? 0 : min;
@@ -93,7 +128,7 @@ class T {
     }
   }
 
-  formatDate(time, format) {
+  formatDate(time, format = 'Y-MM-DD HH:mm:SS') {
     time.constructor !== Date && (time = new Date(time));
     let obj = {
       'Y': time.getFullYear(),
